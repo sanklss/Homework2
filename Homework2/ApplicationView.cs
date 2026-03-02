f6737216e8bd170dece7fabb7817378041988726
sanklss <sanklsswt2@yandex.ru>
2026-03-02 16:39:49 +0300

    Добавление класса ApplicationView

---
+++ Homework2/ApplicationView.cs
@@ -0,0 +1,48 @@
+﻿namespace Homework2
+{
+    public class ApplicationView
+    {
+        public string CheckEmailOrPasswordCommand { get; } = "1";
+        public string CheckUsernameCommand { get; } = "1";
+        public string ExitCommand { get; } = "3";
+
+        private Dictionary<string, string> _commands;
+
+        public ApplicationView()
+        {
+            _commands = new Dictionary<string, string>()
+            {
+                { CheckEmailOrPasswordCommand, "Проверить логин или пароль" },
+                { CheckUsernameCommand, "Проверить правильность имени пользователя" },
+                { ExitCommand, "Выйти" }
+            };
+        }
+
+        public void ShowMenu()
+        {
+            foreach (KeyValuePair<string, string> command in _commands)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"{command.Value} - {command.Key}");
+                Console.ResetColor();
+            }
+        }
+
+        public void ShowInvalidCommandMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Неверная команда!");
+            Console.ResetColor();
+        }
+
+        public string GetUserInput()
+        {
+            return Console.ReadLine();
+        }
+
+        public string GetUserData()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,15 +1,10 @@
-﻿using System.Text.RegularExpressions;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-
-namespace Homework2
+﻿namespace Homework2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
-
-            Console.WriteLine(isUsernameValid);
         }
     }
 }
